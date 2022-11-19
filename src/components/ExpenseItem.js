df3da00c1bef6de2b615bd09c7cef136a0443758
rakshitar1135 <rakshitaraju2000@gmail.com>
2022-11-19 10:29:38 +0530

    Adding Basic Css Styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,15 @@
-function ExpenseItem(){
-    return(<div>
-        <div>March</div>
-        <div>
-            <h2>Car Insurance</h2>
-            <div>$296.99</div>
-        </div>
-    </div>)
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
 }
+
 export default ExpenseItem;
