d2b31a7920c0487e5e3f2c9befee1dc733b3d234
rakshitar1135 <rakshitaraju2000@gmail.com>
2022-11-19 11:27:18 +0530

    EXPENSES JS AND EXPENSES CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import ExpenseItem from './components/ExpenseItem'
-function App(props){
+import Expenses from './components/Expenses';
+function App() {
   const expenses = [
     {
       id: 'e1',
@@ -21,27 +21,13 @@ function App(props){
       date: new Date(2021, 5, 12),
     },
   ];
-return(<div>
-    <h2>Lets Get Started</h2>
-    <ExpenseItem title={expenses[0].title} 
-     amount={expenses[0].amount }
-      date={expenses[0].date}/>
-    <ExpenseItem
-    title={expenses[1].title} 
-    amount={expenses[1].amount }
-     date={expenses[1].date}
-    
-    />
-    <ExpenseItem
-    title={expenses[2].title} 
-    amount={expenses[2].amount }
-     date={expenses[2].date}
-    />
-    <ExpenseItem
-     title={expenses[3].title} 
-     amount={expenses[3].amount }
-      date={expenses[3].date} 
-      />
-  </div>);
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
+    </div>
+  );
 }
+
 export default App;
